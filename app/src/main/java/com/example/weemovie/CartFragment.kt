3fc6687a3d51b1bcc4c318f2c797bc5e126a8a3e
gustavowxt <gustavowxt10@hotmail.com>
2024-11-05 07:31:41 -0300

    tela carrinho vazia, com msg de vazio e botao voltar home

--- app/src/main/java/com/example/weemovie/CartFragment.kt
+++ app/src/main/java/com/example/weemovie/CartFragment.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.weemovie.Adapter.CartAdapter
@@ -21,6 +23,8 @@ class CartFragment : Fragment() {
     private lateinit var totalTextView: TextView
     private lateinit var recyclerView: RecyclerView
     private lateinit var cartAdapter: CartAdapter
+    private lateinit var emptyCartLayout: LinearLayout
+    private lateinit var backToHomeButton: Button
 
     private val cartViewModel: CartViewModel by activityViewModels()
 
@@ -28,63 +32,62 @@ class CartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Infla o layout do fragmento
         val view = inflater.inflate(R.layout.fragment_cart, container, false)
 
-        // Inicializa as views
-        initializeViews(view)
-
-        // Configura o RecyclerView e o Adapter
-        setupRecyclerView(view) // Passa a view inflada como parâmetro
-
-        // Observa as mudanças no carrinho para atualizar a interface
-        observeCartItems()
-
-        // Lida com a finalização do pedido
-        setupFinalizeOrderButton()
-
-        return view
-    }
-
-    private fun initializeViews(view: View) {
         finalizeOrderButton = view.findViewById(R.id.finalizeOrderButton)
         totalTextView = view.findViewById(R.id.totalPriceText)
-    }
+        recyclerView = view.findViewById(R.id.recyclerViewCart)
+        emptyCartLayout = view.findViewById(R.id.emptyCartLayout)
+        backToHomeButton = view.findViewById(R.id.backToHomeButton)
 
-    private fun setupRecyclerView(view: View) { // Adiciona `view` como parâmetro
-        recyclerView = view.findViewById(R.id.recyclerViewCart) // Usa a view passada
-
-        // Passa uma função lambda que utiliza o cartViewModel para manipular a quantidade de produtos
-        cartAdapter = CartAdapter(mutableListOf()) { product, newQuantity ->
-            cartViewModel.updateProductQuantity(product, newQuantity)
-            updateTotalPrice()  // Atualiza o total quando a quantidade é alterada
+        // Configura o RecyclerView e o Adapter
+        cartAdapter = CartAdapter(mutableListOf()) { product, quantity ->
+            cartViewModel.updateProductQuantity(product, quantity)
         }
+        recyclerView.adapter = cartAdapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
 
-        recyclerView.apply {
-            adapter = cartAdapter
-            layoutManager = LinearLayoutManager(context)
-        }
-    }
-
-    private fun observeCartItems() {
+        // Observa as mudanças no carrinho para atualizar a interface
         cartViewModel.cartItems.observe(viewLifecycleOwner) { cartItems ->
-            cartAdapter.updateCartItems(cartItems.toList())  // Converte para List<Pair<Product, Int>>
+            updateCartView(cartItems)
             updateTotalPrice()
         }
-    }
 
-    private fun setupFinalizeOrderButton() {
+        // Botão "Voltar à Home" para o layout vazio
+        backToHomeButton.setOnClickListener {
+            findNavController().navigate(R.id.action_cartFragment_to_homeFragment)
+        }
+
+        // Botão "Finalizar Pedido"
         finalizeOrderButton.setOnClickListener {
-            // Inicia a tela de confirmação de pedido
+            // Iniciar a tela de confirmação de pedido
             val intent = Intent(activity, OrderConfirmationActivity::class.java)
             startActivity(intent)
         }
+
+        return view
     }
 
     private fun updateTotalPrice() {
         val total = cartViewModel.getTotalPrice()
         totalTextView.text = "TOTAL R$ %.2f".format(total)
     }
+
+    private fun updateCartView(cartItems: MutableMap<Product, Int>) {
+        if (cartItems.isEmpty()) {
+            // Exibe o layout do carrinho vazio
+            emptyCartLayout.visibility = View.VISIBLE
+            recyclerView.visibility = View.GONE
+            finalizeOrderButton.visibility = View.GONE
+        } else {
+            // Exibe a RecyclerView e o botão "Finalizar Pedido"
+            emptyCartLayout.visibility = View.GONE
+            recyclerView.visibility = View.VISIBLE
+            finalizeOrderButton.visibility = View.VISIBLE
+            cartAdapter.updateCartItems(cartItems.toList())
+        }
+    }
 }
+
 
 
